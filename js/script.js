8168f3f9f0aab667dc371fef2089a5372347dd97
yeongdon_kim <kyd5022@naver.com>
2023-10-18 23:17:27 +0900

    publication 수정중

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function setInnerHTML() {
+    const element = document.getElementById('my_div');
+    element.innerHTML 
+      = '논문 추가 논문추가';
+  } 
+  
+
+
+
+  function setInnerText() {
+    const element = document.getElementById('my_div');
+    element.innerText 
+      = '논문 추가 논문추가';
+  } 
+  
+
+
+
+
+  function setTextContent() {
+    const element = document.getElementById('my_div');
+    element.textContent 
+      = '논문 추가 논문추가';
+  } 
