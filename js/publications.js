b5f5feb927b220f620be4be1ed243347016728f4
yechoi <annano1@naver.com>
2023-11-01 16:03:53 +0900

    footer 높이 조절

--- js/publications.js
+++ js/publications.js
@@ -85,7 +85,7 @@ function journalALL() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none;"> ${paperInfo}</a><br><br>`;
+    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none;"><span style="font-size:1.5em; color:#999999;"> &#187; </span> ${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
