e2874480682df2e13619393a272243287d0cf04d
yeongdon <kyd5022@naver.com>
2023-11-03 09:16:25 +0900

    insdieTheLab 수정

---
+++ js/top.js
@@ -0,0 +1,18 @@
+// Function to scroll to the top of the page
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // For smooth scrolling (if supported by the browser)
+    });
+}
+
+// Show/hide the "Top" button based on the user's scroll position
+window.onscroll = function() {
+    const topButton = document.getElementById("topButton");
+    
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
