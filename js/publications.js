59f75565a02b9d24aba83bc303a29821578b2a48
yechoi <annano1@naver.com>
2023-11-10 14:57:28 +0900

    publication hover추가

--- js/publications.js
+++ js/publications.js
@@ -85,7 +85,7 @@ function journalALL() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021;  font-size: 0.9em; text-decoration: none; border: none; text-align:justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a href="#" onclick="ALLOpenPaper(${i})" class='paperclick'>${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -93,7 +93,90 @@ function journalALL() {
 }
 
 
+function ALLOpenPaper(paperIndex) {
+  const paperLinks = `https://www.nature.com/articles/s41746-023-00777-z
+  https://pubmed.ncbi.nlm.nih.gov/36073769/
+  https://www.nature.com/articles/s41598-022-15240-4
+  https://ieeexplore.ieee.org/document/9713685
+  https://pubmed.ncbi.nlm.nih.gov/35339844/
+  https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8972117/
+  https://ieeexplore.ieee.org/document/9736959
+  https://www.mdpi.com/1424-8220/22/5/1920
+  https://www.frontiersin.org/articles/10.3389/fphys.2021.808451/full
+  https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=9423964
+  https://pubmed.ncbi.nlm.nih.gov/33973860/
+  https://pubmed.ncbi.nlm.nih.gov/33859568/
+  https://www.mdpi.com/1424-8220/21/6/2188
+  https://pubmed.ncbi.nlm.nih.gov/33762962/
+  https://pubmed.ncbi.nlm.nih.gov/33533723/
+  https://ieeexplore.ieee.org/document/9274312
+  https://ieeexplore.ieee.org/document/9110878
+  https://www.sciencedirect.com/science/article/pii/S2211285520304894
+  https://ieeexplore.ieee.org/document/9075220
+  https://www.nature.com/articles/s41598-020-64033-0
+  https://www.mdpi.com/1424-8220/20/1/33
+  https://www.mdpi.com/1424-8220/19/24/5441
+  https://ieeexplore.ieee.org/document/8599135
+  https://pubmed.ncbi.nlm.nih.gov/31547437/
+  https://link.springer.com/article/10.1007/s42835-019-00185-y
+  https://ieeexplore.ieee.org/document/8556368
+  https://pubmed.ncbi.nlm.nih.gov/30398064/
+  https://lib.jucs.org/article/23522/
+  https://pubmed.ncbi.nlm.nih.gov/29790159/
+  https://pubmed.ncbi.nlm.nih.gov/30210363/
+  https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6052119/
+  https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6022123/
+  https://pubmed.ncbi.nlm.nih.gov/28169836/
+  https://ieeexplore.ieee.org/document/7903646
+  https://pubmed.ncbi.nlm.nih.gov/28169836/
+  https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5234121/
+  https://pubmed.ncbi.nlm.nih.gov/26511754/
+  https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0150144
+  https://ieeexplore.ieee.org/document/7273819
+  https://www.researchgate.net/publication/282923577_A_Localization_Method_for_First_and_Second_Heart_Sounds_Based_on_Energy_Detection_and_Interval_Regulation
+  https://ieeexplore.ieee.org/abstract/document/6837453
+  https://biomedical-engineering-online.biomedcentral.com/articles/10.1186/1475-925X-13-116
+  https://pubmed.ncbi.nlm.nih.gov/24693921/
+  https://pubmed.ncbi.nlm.nih.gov/24616586/
+  https://ieeexplore.ieee.org/abstract/document/5985462
+  https://pubmed.ncbi.nlm.nih.gov/21806292/
+  https://ieeexplore.ieee.org/abstract/document/5571922
+  https://pubmed.ncbi.nlm.nih.gov/20703930/
+  https://www.sciencedirect.com/science/article/pii/S0010482509001826
+  https://biomedical-engineering-online.biomedcentral.com/articles/10.1186/1475-925X-8-31
+  https://pubmed.ncbi.nlm.nih.gov/19441955/
+  https://www.researchgate.net/publication/245405213_Adaptive_motion_artifacts_reduction_algorithm_for_ECG_signal_in_textile_wearable_sensor
+  https://www.kci.go.kr/kciportal/ci/sereArticleSearch/ciSereArtiView.kci?sereArticleSearchBean.artiId=ART002704114
+  https://www.researchgate.net/publication/338952190_Feasibility_Study_of_Ultra-Short-Term_Heart_Rate_Variability_in_Recovery_Condition_after_Exercise
+  https://www.researchgate.net/publication/338278472_Comparison_of_Classifier_for_Pain_Assessment_based_on_Photoplethysmogram_and_Machine_Learning
+  https://www.researchgate.net/publication/338278476_Force_Sensor_and_MSP430-based_System_for_Motion_Detection_During_Sleep
+  https://www.researchgate.net/publication/336948869_Postoperative_Pain_Assessment_Features_Based_on_Derivative_Photoplethysmogram
+  https://www.researchgate.net/publication/327438305_Investigation_of_the_pulse_to_pulse_interval_and_amplitude_variation_of_photoplethymogram_as_a_pain_assessment_parameter_during_anesthseia
+  http://journal.auric.kr/kiee/XmlViewer/f379178
+  https://www.researchgate.net/publication/326153933_Investigation_of_the_Feasibility_of_Postoperative_Pain_Assessment_by_Analysing_Photoplethysmogram_Variability_Analysis
+  https://www.dbpia.co.kr/journal/articleDetail?nodeId=NODE07284296
+  https://koreascience.kr/article/JAKO201734158605923.page
+  https://www.researchgate.net/publication/307913747_Reconstruction_of_the_Undersampled_Photoplethysmogram_with_Various_Interpolation_Methods
+  https://koreascience.kr/article/JAKO201621650895994.page
+  https://koreascience.kr/article/JAKO201734158605923.page
+  https://www.researchgate.net/publication/307913747_Reconstruction_of_the_Undersampled_Photoplethysmogram_with_Various_Interpolation_Methods
+  https://www.researchgate.net/publication/273911498_Analysis_for_the_Fluctuation_of_the_Photoplethysmographic_Waveform_derived_by_Temperature_Stress_of_Measuring_Position
+  https://www.researchgate.net/publication/283032083_Evaluation_of_the_Ambient_Temperature_Effect_for_the_Autonomic_Nervous_Activity_through_the_Time_Domain_Analysis_of_the_Heart_Rate_Variability
+  https://www.researchgate.net/publication/283032236_Evaluation_of_the_Ambient_Temperature_Effect_for_the_Autonomic_Nervous_Activity_of_the_Young_Adult_through_the_Frequency_Analysis_of_the_Heart_Rate_Variability
+  https://www.kci.go.kr/kciportal/ci/sereArticleSearch/ciSereArtiView.kci?sereArticleSearchBean.artiId=ART001894609
+  https://www.ingentaconnect.com/contentone/asp/asl/2012/00000018/00000001/art00015
+  https://www.ingentaconnect.com/content/asp/asl/2012/00000006/00000001/art00011
+  https://koreascience.kr/article/JAKO201123457285030.page
+  https://www.semanticscholar.org/paper/Respiration-Measurement-System-using-Textile-Sensor-Min-Yun/466825ed67219f0bbb3c1f5e5458be1ca7db2b37
+  https://www.researchgate.net/publication/263401299_Down-Scaled_3D_Object_for_Telediagnostic_Imaging_Support_System
+  https://synapse.koreamed.org/articles/1103183`; // 각 논문에 대한 실제 링크 URL
 
+  const linksArray = paperLinks.split('\n'); // 띄어쓰기를 기준으로 URL 분할
+
+  if (paperIndex >= 0 && paperIndex < linksArray.length) {
+    window.location.href = linksArray[paperIndex]; // 클릭 시 해당 URL로 이동
+  }
+}
 
 
 
@@ -160,7 +243,7 @@ function journalSCI() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a href="#" onclick="SCIOpenPaper(${i})" style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a href="#" onclick="SCIOpenPaper(${i})" class='paperclick'>${paperInfo}</a><br><br>`;
 
   }
 
@@ -263,7 +346,7 @@ function journalSCOPUS() {
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
 
-    paperList += `<a href="#" onclick="SCOPUSOpenPaper(${i})" style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;"> ${paperInfo}</a><br><br>`;
+    paperList += `<a href="#" onclick="SCOPUSOpenPaper(${i})" class='paperclick'"> ${paperInfo}</a><br><br>`;
 
 
   }
@@ -319,7 +402,7 @@ function journalKCI() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;"> ${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'"> ${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -495,7 +578,7 @@ function confernceAll() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'>${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -568,7 +651,7 @@ function confernceInter() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;"> ${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'"> ${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -693,7 +776,7 @@ function confernceDome() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'">${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -848,7 +931,7 @@ function patentAll() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'">${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -930,7 +1013,7 @@ function patentInter() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'">${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -960,7 +1043,7 @@ function patentDesignInter() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'>${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -1035,7 +1118,7 @@ function patentDome() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'>${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -1067,7 +1150,7 @@ function patentDesignDome() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; text-align: justify;">${paperInfo}</a><br><br>`;
+    paperList += `<a class='paperlist'>${paperInfo}</a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
@@ -1117,7 +1200,7 @@ function registeredSW() {
   let paperList = '';
   for (let i = 0; i < paperArray.length; i++) {
     const paperInfo = paperArray[i];
-    paperList += `<a style="color: #1c2021; font-size : 0.9em; text-decoration: none; border: none; "><span style = " text-align: justify;">${paperInfo}</span></a><br><br>`;
+    paperList += `<a class='paperlist'>${paperInfo}</span></a><br><br>`;
   }
 
   const element = document.getElementById('my_div');
