30675c8401418f98074aa77500fb025153c82522
yeongdon <kyd5022@naver.com>
2023-11-15 13:07:59 +0900

    research 애니메이션 수정

--- js/research.js
+++ js/research.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     $(".nav-link-research").click(function () {
-        // Extract section and type from the clicked link
         var linkText = $(this).text().toLowerCase();
         var section = linkText.substring(0, linkText.indexOf(' '));
         var type = linkText.substring(linkText.indexOf(' ') + 1).toLowerCase();
 
-        // Call the toggleParagraph function with section and type
         toggleParagraph(section, type);
     });
 });
@@ -14,19 +12,19 @@ function toggleParagraph(sectionId, type) {
     var paperParagraph = $("#" + sectionId + '-paper');
     var patentParagraph = $("#" + sectionId + '-patent');
 
+    var deferred = $.Deferred();
+
     if (type === 'paper') {
-        if (paperParagraph.is(":visible")) {
-            paperParagraph.slideUp(500);
-        } else {
-            paperParagraph.slideDown(500);
-            patentParagraph.slideUp(500);
-        }
+        patentParagraph.slideUp(500, function () {
+            paperParagraph.slideToggle(500, deferred.resolve);
+        });
     } else if (type === 'patent') {
-        if (patentParagraph.is(":visible")) {
-            patentParagraph.slideUp(500);
-        } else {
-            patentParagraph.slideDown(500);
-            paperParagraph.slideUp(500);
-        }
+        paperParagraph.slideUp(500, function () {
+            patentParagraph.slideToggle(500, deferred.resolve);
+        });
     }
+
+    // $.when(deferred).done(function () {
+    //     // Code to execute after the animation is complete (if needed)
+    // });
 }
