4e77d189cf8f3bc204a2baeea4b39ec229883e39
yeongdon <kyd5022@naver.com>
2023-12-01 10:33:20 +0900

    모바일 수정

---
+++ js/slick.control.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function () {
+    if ($(window).width() > 480) {
+        // Initialize Slick slider only if the window width is greater than 480px
+        $('.blog-slider').slick({
+            dots: true,
+            infinite: true,
+            mobileFirst: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true, // Show arrows for navigation
+            prevArrow: "<button type='button' class='slick-prev'>Previous</button>",
+            nextArrow: "<button type='button' class='slick-next'>Next</button>",
+            autoplay: true,
+            autoplaySpeed: 3500, // Adjust the speed as needed
+            pauseOnHover: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    }
+});
